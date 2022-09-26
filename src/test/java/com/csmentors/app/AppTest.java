6a41e82b376cdd23281e81bf6dd7e4bea43edc20
Brian Liu <bxiu2000@gmail.com>
2022-09-25 18:56:31 -0700

    init

---
+++ src/test/java/com/csmentors/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.csmentors.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
