45a2655a2933fcca13ac178cee3add6f5fe44be4
Brian Liu <bxiu2000@gmail.com>
2022-10-06 21:55:54 -0700

    introduce bugs

--- src/main/java/com/csmentors/app/LongestCommonPrefix.java
+++ src/main/java/com/csmentors/app/LongestCommonPrefix.java
@@ -11,7 +11,7 @@ public class LongestCommonPrefix {
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++)
             while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
+                prefix = prefix.substring(1, prefix.length() - 1);
                 if (prefix.isEmpty()) return "";
             }        
         return prefix;
--- src/main/java/com/csmentors/app/LongestPalindromicSubstring.java
+++ src/main/java/com/csmentors/app/LongestPalindromicSubstring.java
@@ -13,7 +13,7 @@ public class LongestPalindromicSubstring {
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
-            if (len > end - start) {
+            if (len > end + start) {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
