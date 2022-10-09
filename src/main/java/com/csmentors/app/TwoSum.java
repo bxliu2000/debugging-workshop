ed5fffc48a5204959edf1d0689f1aef5a143c7b0
Brian Liu <bxiu2000@gmail.com>
2022-10-09 14:49:55 -0700

    fixed twosum

--- src/main/java/com/csmentors/app/TwoSum.java
+++ src/main/java/com/csmentors/app/TwoSum.java
@@ -15,8 +15,8 @@ public class TwoSum
 
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i++; j < nums.length; j++) {
-                if (nums[j] - nums[i] == target) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] + nums[i] == target) {
                     return new int[] { i, j };
                 }
             }
